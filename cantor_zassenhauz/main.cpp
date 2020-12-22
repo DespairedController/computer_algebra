c778a5892f56c25e1bf8cc72e8b6a50775efaccd
DespairedController <ghjnhesm@gmail.com>
2020-12-22 22:26:26 +0300

    main fixed

--- cantor_zassenhauz/main.cpp
+++ cantor_zassenhauz/main.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
 #include <tuple>
 #include "test.h"
 
 using namespace Givaro;
 
 int main() {
-    std::cout << "q,d,time" << std::endl;
     std::vector<std::tuple<long, long, long>> ans;
     CantorZassenhaus::GaloisField Z3(7, 1);  // integers modulo 13
     Poly1Dom<GFqDom<int>, Dense> DP29(Z3, Indeter("X"));
+    Test::run_random_tests(DP29);
 }
